972d54f87aadbe6f0b32b24484b833fcd3eb8829
FATEH07 <fatehsingh07462@gmail.com>
2025-03-28 21:59:00 +0530

    sdfds

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    port:  5173,  // Set port dynamically
+    host: '0.0.0.0'                  // Allow external access
+  }
 })
